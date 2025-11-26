cdd6658f3ca4ef28d4a00b19f1fad19f79c9345d
C-DGameMaker <charlotte5634@outlook.com>
2025-11-26 14:00:21 -0400

    You Can Answers questions

--- CharlieDobson_GameShowQuiz_Programming1/Program.cs
+++ CharlieDobson_GameShowQuiz_Programming1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace CharlieDobson_GameShowQuiz_Programming1
 {
@@ -15,14 +16,27 @@ namespace CharlieDobson_GameShowQuiz_Programming1
 
         static bool isPlaying;
         static bool isPlayingAgain;
+        static bool questionAnswer = false;
 
-        static string[] questions = { };
-        static string[,] answers = { { } };
+        static char input = ' ';
 
+        static string[] questions = {"What color is my shirt?", "What color is the sky?", "..."};
+        static char[] acceptableAnswer = {'1', '2', '3', '4' };
+        static string[,] answers = 
+        { 
+            {"[1] Green", "[2] Gray", "[3] Blue", "[4] Red"},
+            {"[1] Green", "[2] Gray", "[3] Blue", "[4] Red"},
+            {"[1] Green", "[2] Gray", "[3] Blue", "[4] Red"},
+            {"[1] Green", "[2] Gray", "[3] Blue", "[4] Red"},
+            {"[1] Green", "[2] Gray", "[3] Blue", "[4] Red"},
+            {"[1] Green", "[2] Gray", "[3] Blue", "[4] Red"},
+        };
+
+        static char[] actualAnswers = {'1', '1', '1' };
         //╔╗═╚╝║
         static void Main(string[] args)
         {
-            Intro();
+            //Intro();
             Console.Clear();
             isPlaying = true;
             while (isPlaying == true)
@@ -49,6 +63,9 @@ namespace CharlieDobson_GameShowQuiz_Programming1
                 }
 
             }
+
+            Console.Clear();
+            Console.ReadKey(true);
         }
 
         static void Intro()
@@ -79,9 +96,59 @@ namespace CharlieDobson_GameShowQuiz_Programming1
 
         static void Questions()
         {
-            for(int i = 0; i < 10; i++)
+            for(int i = 0; i < questions.Length; i++)
             {
+                questionAnswer = false;
+                while(questionAnswer == false)
+                {
+                    Console.Write("╔═════════════════════════════════════╗\n");
+                    Console.Write("║");
+                    Console.Write($"Question {i + 1}/{questions.Length}: {questions[i]}");
+                    Console.ResetColor();
+                    Console.Write("║ \n");
+                    Console.WriteLine("║-------------------------------------║");
+                    for(int j = 0; j < answers.GetLength(1); j++)
+                    {
+                        Console.Write("║    ");
+                        Console.Write(answers[i, j]);
+                        Console.Write("                          ║ \n");
+                    }
+                    Console.Write("╚═════════════════════════════════════╝\n");
+                    Console.WriteLine("What is the answer?");
+                    Console.Write("Your answer: ");
 
+                    input = Console.ReadKey().KeyChar;
+                    if (!acceptableAnswer.Contains((char)(input)))
+                    {
+                        Console.Write("PLEASE PUT ACCEPTABLE ANSWER!");
+                        Thread.Sleep(10000);
+                        Console.Clear();
+
+                    }
+
+                    if(input == actualAnswers[i])
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("CORRECT!");
+                        totalCorrectAnswers++;
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("INCORRECT!");
+                        Console.ResetColor();
+                    }
+
+
+
+
+                    Console.ReadKey(true);
+                    questionAnswer = true;
+                }
+                
             }
         }
     }
